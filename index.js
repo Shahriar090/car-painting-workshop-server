779299466ffa158eda546fe54372fe5027bdec7c
Shahriar090 <shahriarh655@gmail.com>
2023-08-15 22:41:23 +0600

    Added api to load single data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -34,7 +34,19 @@ async function run() {
         const cursor = servicesCollection.find();
         const result = await cursor.toArray();
         res.send(result);
-    })
+    });
+
+    // load single data
+
+    app.get('/services/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const options = {
+       projection: { title: 1, price : 1, service_id : 1 },
+      };
+      const result = await servicesCollection.findOne(query, options);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
